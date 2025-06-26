6f5d845ae986678cee265e4e4ee05c91b1c8e645
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-26 22:53:07 +0200

    aplicamos funcionalidad al formulario

--- src/Components/FormHooks.jsx
+++ src/Components/FormHooks.jsx
@@ -1,8 +1,30 @@
 import {useState} from 'react';
 
 export const FormHooks = () => {
+
+  const [stateForm, setStateForm] = useState({
+    email : '',
+    username : '',
+    password : ''
+  })
+
+  const { email, username, password } = stateForm;
+
+  const handleForm = ( { target } ) => {
+      const { name, value } = target;
+    setStateForm({
+      ...stateForm,
+      [ name ] : value
+    })
+  }
+
+  const onSubmitForm = ( event ) => {
+    event.preventDefault()
+    console.log( stateForm )
+  }
+
   return (
-    <form>
+    <form onSubmit={ onSubmitForm }>
 
       <div className="form-group">
         <label htmlFor="email">Email</label>
@@ -12,6 +34,8 @@ export const FormHooks = () => {
           className="form-control"
           name="email"
           placeholder="Enter email"
+          value={ email }
+          onChange={ handleForm }
         />
       </div>
 
@@ -23,6 +47,8 @@ export const FormHooks = () => {
           className="form-control"
           name="username"
           placeholder="Username"
+          value={ username }
+          onChange={ handleForm }
         />
       </div>
 
@@ -34,6 +60,8 @@ export const FormHooks = () => {
           className="form-control"
           name="password"
           placeholder="Password"
+          value={ password }
+          onChange={ handleForm }
         />
       </div>
 

713130dcdffa80ac9c734340b8f1459ab70a71e3
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-26 22:42:47 +0200

    creamos formulario con bootstrap

--- src/Components/FormHooks.jsx
+++ src/Components/FormHooks.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import {useState} from 'react';
 
 export const FormHooks = () => {
   return (
-    <div>FormHooks</div>
+    <form>
+
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+
+        <input
+          type="email"
+          className="form-control"
+          name="email"
+          placeholder="Enter email"
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="username">UserName</label>
+
+        <input
+          type="text"
+          className="form-control"
+          name="username"
+          placeholder="Username"
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          placeholder="Password"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
   )
 }
